059afa24ba43842e34c39efa824d0d23f3b42ca2
KillAura7170 <83659309+KillAura7170@users.noreply.github.com>
2023-04-26 00:18:30 +0900

    Add files via upload

---
+++ stop_tor_proxie.py
@@ -0,0 +1,4 @@
+import os
+
+print("##### TOR AS PROXY BY KILLAURA #####" "\nstopping...")
+os.system("sudo killall torproxie")
---
+++ tor_proxie.py
@@ -0,0 +1,5 @@
+import os
+
+print("Tor as Proxie!" "\nMade By KillAura" "\nstarting..." "\nMake sure that you have 127.0.0.1:9050 in proxie settings")
+
+os.system("sudo tor")
